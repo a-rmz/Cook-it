38bcae063fc3b7582362978b7622188c7d1dab4c
a-rmz <armzprz@gmail.com>
2016-03-23 15:16:13 -0600

    Removed problems with FAB

--- app/src/main/java/com/dangerducks/cookit/MainActivity.java
+++ app/src/main/java/com/dangerducks/cookit/MainActivity.java
@@ -3,6 +3,7 @@ package com.dangerducks.cookit;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -26,12 +27,15 @@ public class MainActivity extends AppCompatActivity{
     private String mActivityTitle;
     Toolbar toolbar;
     private ImageView taco;
+    private FloatingActionButton fab;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setupToolbar();
