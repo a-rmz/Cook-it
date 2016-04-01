4847150dce488048adb0fe55fe396b6fb8fd3707
a-rmz <armzprz@gmail.com>
2016-04-01 15:40:03 -0600

    Cards showing in RecyclerView with adapter

--- app/src/main/java/com/dangerducks/cookit/MainActivity.java
+++ app/src/main/java/com/dangerducks/cookit/MainActivity.java
@@ -3,25 +3,33 @@ package com.dangerducks.cookit;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.SwipeDismissBehavior;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.dangerducks.cookit.kitchen.Recipe;
 import com.dangerducks.cookit.utils.FileManager;
+import com.dangerducks.cookit.utils.RecipeAdapter;
 
 
 /**
@@ -29,13 +37,16 @@ import com.dangerducks.cookit.utils.FileManager;
  */
 public class MainActivity extends AppCompatActivity{
 
-    private DrawerLayout drawerLayout;
+    private CoordinatorLayout coordinatorLayout;
     private NavigationView navigationView;
     private ActionBarDrawerToggle mDrawerToggle;
     private String mActivityTitle;
     Toolbar toolbar;
     private ImageView taco;
     private FloatingActionButton fab;
+    RecyclerView recyclerView;
+    SwipeDismissBehavior<FloatingActionButton> swipe;
+    CardView cardView;
 
 
     @Override
@@ -54,18 +65,24 @@ public class MainActivity extends AppCompatActivity{
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setupToolbar();
 
-        drawerLayout = (DrawerLayout)findViewById(R.id.main_drawer_layout);
+        coordinatorLayout = (CoordinatorLayout)findViewById(R.id.main_coordinator_layout);
         setUpDrawer();
 
         User.user().recipesSaved = FileManager.getRecipes(getFilesDir().getPath());
-        Log.v("USER RECIPES", "Recipe quant " + User.user().recipesSaved.size());
-        showRecipes();
+
+        recyclerView = (RecyclerView) findViewById(R.id.card_recipe_container);
+        setupSwiper();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setupRecyclerView();
+    }
 
     private void setUpDrawer() {
-        drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
-        navigationView = (NavigationView) findViewById(R.id.main_drawer);
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+        navigationView = (NavigationView) drawerLayout.findViewById(R.id.main_drawer);
 
         mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer) {
             public void onDrawerOpened(View view) {
@@ -173,32 +190,32 @@ public class MainActivity extends AppCompatActivity{
 
     private void addRecipe() {
         Intent intent = new Intent(MainActivity.this, AddRecipe.class);
-        startActivityForResult(intent, 0);
+        startActivity(intent);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        showRecipes();
+    private void setupRecyclerView() {
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        RecipeAdapter adapter = new RecipeAdapter(User.user().recipesSaved);
+        recyclerView.setAdapter(adapter);
     }
 
-    private void showRecipes() {
-        if(!User.user().recipesSaved.isEmpty()) {
-            Log.v("SHOW RECIPES", "Show recipes called");
-            LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
-            LinearLayout container = (LinearLayout) findViewById(R.id.card_recipe_container);
-            container.removeAllViews();
-            View view;
-            TextView header, subtitle;
-            for (Recipe recipe : User.user().recipesSaved) {
-                view = inflater.inflate(R.layout.recipe_card, null);
-                header = (TextView) view.findViewById(R.id.card_header);
-                subtitle = (TextView) view.findViewById(R.id.card_show_portions);
-                header.setText(recipe.getName());
-                subtitle.setText(recipe.getPortions() + " portions");
-                container.addView(view);
+    private void setupSwiper() {
+        ItemTouchHelper.SimpleCallback callback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.START | ItemTouchHelper.END) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
             }
-        }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                Snackbar.make(coordinatorLayout, "Done", Snackbar.LENGTH_LONG).show();
+            }
+
+        };
+        ItemTouchHelper helper = new ItemTouchHelper(callback);
+        helper.attachToRecyclerView(recyclerView);
     }
 }
 
---
+++ app/src/main/java/com/dangerducks/cookit/utils/RecipeAdapter.java
@@ -0,0 +1,62 @@
+package com.dangerducks.cookit.utils;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.dangerducks.cookit.R;
+import com.dangerducks.cookit.kitchen.Recipe;
+
+import java.util.Vector;
+
+/**
+ * Created by alex on 4/1/16.
+ */
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHolder> {
+
+    Vector<Recipe> recipes;
+
+
+    public static class RecipeHolder extends RecyclerView.ViewHolder {
+        CardView holder;
+        TextView title, portions;
+
+        public RecipeHolder(View view)  {
+            super(view);
+            holder = (CardView) view.findViewById(R.id.recipe_card_layout);
+            title = (TextView) view.findViewById(R.id.card_header);
+            portions = (TextView) view.findViewById(R.id.card_show_portions);
+        }
+    }
+
+
+    public RecipeAdapter(Vector<Recipe> recipes) {
+        super();
+        this.recipes = recipes;
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes.size();
+    }
+
+    @Override
+    public RecipeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_card, parent, false);
+        return new RecipeHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecipeHolder holder, int position) {
+        holder.title.setText(recipes.get(position).getName());
+        holder.portions.setText(recipes.get(position).getPortions() + " portions");
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+}
