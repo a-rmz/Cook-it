dda92959bf6005aebc960ee97312bf1472725755
Rogelio Miguel Hernández <par9615@gmail.com>
2016-04-03 20:50:32 -0500

    Methods added

--- app/src/main/java/com/dangerducks/cookit/kitchen/Category.java
+++ app/src/main/java/com/dangerducks/cookit/kitchen/Category.java
@@ -15,11 +15,38 @@ public class Category implements Serializable {
     public static int recipeQuantity;
     Vector<Recipe> recipes;
 
+
+    public Category(){
+
+    }
+
     public Category(String name) {
         this.name = name;
     }
 
-    void sortRecipes() {
+    public void sortRecipes() {
         // Sort magic
     }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDescription(String desc){
+        this.description = desc;
+    }
+
+    public void addRecipe(Recipe recipe){
+        this.recipes.add(recipe);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+
 }
