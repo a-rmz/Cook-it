5a748eae5feae3a9c49804a12b60ad4ee75fcf10
a-rmz <armzprz@gmail.com>
2016-05-08 18:40:27 -0500

    Basic search

--- app/src/main/java/com/dangerducks/cookit/MainActivity.java
+++ app/src/main/java/com/dangerducks/cookit/MainActivity.java
@@ -1,14 +1,24 @@
 package com.dangerducks.cookit;
 
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 
+import com.dangerducks.cookit.DB.local.IngredientContract;
+import com.dangerducks.cookit.DB.local.LocalDBOperations;
 import com.dangerducks.cookit.utils.FileManager;
 import com.github.clans.fab.FloatingActionMenu;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -17,23 +27,32 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.support.v7.widget.SearchView;
+import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dangerducks.cookit.kitchen.Recipe;
 import com.dangerducks.cookit.utils.RecipeAdapter;
 import com.mysql.jdbc.jdbc2.optional.SuspendableXAConnection;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Vector;
 
 
 /**
  * Created by alex on 2/29/16.
  */
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private static final int RECIPE_ACTIVITY = 1;
     private static final int RECIPE_REMOVED = -1;
@@ -50,6 +69,8 @@ public class MainActivity extends AppCompatActivity{
     NavigationView navigationView;
     Vector<Recipe> displayableRecipes;
     RecipeAdapter adapter;
+    SearchView searchView;
+    MenuItem searchItem;
 
 
     @Override
@@ -201,9 +222,7 @@ public class MainActivity extends AppCompatActivity{
         }
 
         switch (menuItem.getItemId()) {
-            case R.id.action_search:
-                nothingToDoHere();
-                return true;
+
             case R.id.action_credits:
                 AboutDialog aboutDialog = new AboutDialog(this);
                 aboutDialog.setTitle(getResources().getString(R.string.about));
@@ -222,7 +241,24 @@ public class MainActivity extends AppCompatActivity{
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity_actions, menu);
 
+
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchItem = menu.findItem(R.id.action_search);
+        searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setSearchableInfo((
+                searchManager.getSearchableInfo(
+                        new ComponentName(this, MainActivity.class)
+                )));
+
+        searchView.setIconifiedByDefault(false);
+        searchView.setOnQueryTextListener(this);
+
         return super.onCreateOptionsMenu(menu);
+    }
+
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
     }
 
     public void nothingToDoHere() {
@@ -277,5 +313,73 @@ public class MainActivity extends AppCompatActivity{
         }
     }
 
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        MenuItemCompat.collapseActionView(searchItem);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        ArrayList<Recipe> found = new ArrayList<>();
+        for(Recipe r : User.user().recipesSaved) {
+            if(r.getName().toLowerCase().contains(newText.toLowerCase()) && newText.length() > 0) {
+                found.add(r);
+            }
+        }
+
+        // Cursor
+        String[] columns = new String[] { "_id", "text", "int" };
+        Object[] temp = new Object[] { 0, "default", 0 };
+
+        MatrixCursor cursor = new MatrixCursor(columns);
+
+        for(int i = 0; i < found.size(); i++) {
+
+            temp[0] = i;
+            temp[1] = found.get(i).getName();
+            temp[2] = found.get(i).getPortions();
+            cursor.addRow(temp);
+
+        }
+
+        // SearchView
+
+        searchView.setSuggestionsAdapter(new CursorAdapter(this, cursor, false) {
+            @Override
+            public View newView(Context context, Cursor cursor, ViewGroup parent) {
+                return LayoutInflater.from(context).inflate(R.layout.recipe_card, parent, false);
+            }
+
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+                TextView tv = (TextView) view.findViewById(R.id.card_header);
+                tv.setText(cursor.getString(1));
+                tv = (TextView) view.findViewById(R.id.card_show_portions);
+                tv.setText(cursor.getInt(2) + " portions");
+
+                Recipe rec = null;
+                for(Recipe r : User.user().recipesSaved) {
+                    if(r.getName().equals(cursor.getString(1))) {
+                        rec = r;
+                    }
+                }
+                final Recipe recipe = rec;
+
+                view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(v.getContext(), ShowRecipe.class);
+                        intent.putExtra("recipe", recipe);
+                        searchView.clearFocus();
+                        MenuItemCompat.collapseActionView(searchItem);
+                        ((Activity) v.getContext()).startActivityForResult(intent, 1, null);
+                    }
+                });
+            }
+        });
+
+        return false;
+    }
 }
 
